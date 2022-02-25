4af1bb59b10c6a910ce27e31e397f5fd89037b73
secretpray <a.p.ruban@gmail.com>
2022-02-25 09:43:02 +0200

    Add role, Create room only admin

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,5 @@
 class RoomsController < ApplicationController
+
   def index
     @rooms = Room.order(created_at: :desc)
     @new_room = Room.new
@@ -17,10 +18,28 @@ class RoomsController < ApplicationController
   end
 
   def create
+    return if not_autorize?
+
     @new_room = Room.new(user: current_user)
 
     if @new_room.save
+      flash.now[:notice] = "Room created!"
+      turbo_render_flash(flash)
       @new_room.broadcast_prepend_to :rooms, partial: "rooms/room", locals: { user: current_user, room: @new_room }
     end
+  end
+
+  private
+
+  def not_autorize?
+    unless current_user&.admin?
+      flash.now[:alert] = "Not autorized!"
+      turbo_render_flash(flash)
+      return true
+    end
+  end
+
+  def turbo_render_flash(flash)
+    Turbo::StreamsChannel.broadcast_update_to([current_user, :rooms], target: 'flash_container', partial: 'shared/flash', locals: { flash: flash })
   end
 end
